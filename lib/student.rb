ba56bb2b8cb9aea10f03fa01b37cfe60c66dce6d
AmSmo <adam.smolenski@gmail.com>
2020-08-07 12:56:19 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
 
+    attr_accessor :knowledge
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(learnt)
+        knowledge << learnt
+    end
 end
