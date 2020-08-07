11d548f350b28a98ae521d39ceb887e93cceda83
alerner11 <alyssaplerner@gmail.com>
2020-08-07 13:09:27 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
 
+    def learn(string)
+        knowledge << string
+    end
 end
