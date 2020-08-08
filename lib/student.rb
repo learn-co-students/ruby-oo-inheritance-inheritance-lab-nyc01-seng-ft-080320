f1b9a801cae87c5b5e018c4ea1e5d6c4421eba01
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-08 11:53:39 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
+    # attrs section
+    attr_writer :knowledge
 
+    # initialization
+    def initialize
+        self.knowledge = []
+    end
+
+    # instance methods
+    def learn(language)
+        self.knowledge << language
+    end
+
+
+    # custom reader/writer
+
+    def knowledge
+        @knowledge
+    end
 end
