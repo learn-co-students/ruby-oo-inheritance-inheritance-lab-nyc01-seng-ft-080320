bdb83a1feaf71c6a2637902e043254269ed8a5cc
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-09 10:16:25 -0400

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  attr_accessor :first_name, :last_name
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
 
 end
