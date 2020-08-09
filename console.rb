41eaafee39f7de5b72229638a8cfd84a994cacad
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-09 10:24:10 -0400

    Done.

--- console.rb
+++
@@ -1,6 +0,0 @@
-require_relative './lib/teacher'
-require_relative './lib/student'
-require_relative './lib/user'
-require 'pry'
-
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,4 @@
 class Student < User
-  attr_accessor :first_name, :last_name
   attr_reader :knowledge
 
   def initialize
