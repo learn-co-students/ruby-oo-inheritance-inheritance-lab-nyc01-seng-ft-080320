9c2fc4ceac2bd8e54329dcfa778f922ab84dc5bd
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-10 00:25:47 -0400

    completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+    attr_accessor :knowledge
+    def initialize 
+        @knowledge  = Array.new
+    end
+
+    def learn new_knowledge
+        @knowledge << new_knowledge
+    end
 
 end
