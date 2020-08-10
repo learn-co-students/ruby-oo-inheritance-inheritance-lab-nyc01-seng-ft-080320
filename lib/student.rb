f2ef349f08eecc97d82f80344554ee41ea99ca85
AJavier55 <aliciajavier5@gmail.com>
2020-08-10 09:29:47 -0400

    teacher/student inherited from user class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
+require_relative './user.rb'
 class Student < User
 
+  def initialize
+    @knowledge = []
+  end 
+
+  def learn(learned)
+    @knowledge << learned
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
