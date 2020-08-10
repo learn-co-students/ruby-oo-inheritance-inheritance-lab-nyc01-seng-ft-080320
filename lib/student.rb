0fecde96ab046ed19c4ab40c29601be2a0cfcdaa
royFloresNyc <royfloresnyc@gmail.com>
2020-08-09 20:49:41 -0400

    Complete Lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(new_info_string)
+        @knowledge << new_info_string
+    end 
+
+    def knowledge
+        @knowledge
+    end 
 end
